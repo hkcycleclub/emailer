40a36611d2dbe7c22cb8b4d64b952d520d69c3ce
Mia, Mashrur (DI PA SW DH APM 4 1) <mashrur.mia@siemens.com>
2024-11-03 18:19:36 +0000

    Format code

--- src/main/java/com/hkcycleclub/email/RecipientParser.java
+++ src/main/java/com/hkcycleclub/email/RecipientParser.java
@@ -1,50 +1,46 @@
 package com.hkcycleclub.email;
 
-import org.supercsv.cellprocessor.constraint.NotNull;
-import org.supercsv.cellprocessor.constraint.StrRegEx;
-import org.supercsv.cellprocessor.ift.CellProcessor;
-import org.supercsv.io.CsvBeanReader;
-import org.supercsv.prefs.CsvPreference;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.constraint.StrRegEx;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
+
 public class RecipientParser {
-    public static List<Recipient> parseCsv(String filename) throws IOException {
+  public static List<Recipient> parseCsv(String filename) throws IOException {
 
-        List<Recipient> retVal = new ArrayList<>();
+    List<Recipient> retVal = new ArrayList<>();
 
-        Reader inputReader = new InputStreamReader(RecipientParser.class.getResourceAsStream(filename));
-        CsvBeanReader csvBeanReader =
-                new CsvBeanReader(inputReader, CsvPreference.STANDARD_PREFERENCE);
-        csvBeanReader.getHeader(true);
+    Reader inputReader = new InputStreamReader(RecipientParser.class.getResourceAsStream(filename));
+    CsvBeanReader csvBeanReader = new CsvBeanReader(inputReader, CsvPreference.STANDARD_PREFERENCE);
+    csvBeanReader.getHeader(true);
 
-        final CellProcessor[] processors = getProcessors();
+    final CellProcessor[] processors = getProcessors();
 
-        final String[] header = {
-                "fullName",
-                "emailAddress"
-        };
+    final String[] header = {"fullName", "emailAddress"};
 
-        Recipient applicant;
-        while ((applicant = csvBeanReader.read(Recipient.class, header, processors)) != null) {
-            retVal.add(applicant);
-        }
-
-        return retVal;
+    Recipient applicant;
+    while ((applicant = csvBeanReader.read(Recipient.class, header, processors)) != null) {
+      retVal.add(applicant);
     }
 
-    private static CellProcessor[] getProcessors() {
+    return retVal;
+  }
 
-        final String emailRegex = "[a-z0-9\\._]+@[a-z0-9\\.]+"; // just an example, not very robust!
-        StrRegEx.registerMessage(emailRegex, "must be a valid email address");
+  private static CellProcessor[] getProcessors() {
 
-        return new CellProcessor[]{
-                new NotNull(), // Full Name
-                new NotNull()  // Email address
-        };
-    }
+    final String emailRegex = "[a-z0-9\\._]+@[a-z0-9\\.]+"; // just an example, not very robust!
+    StrRegEx.registerMessage(emailRegex, "must be a valid email address");
+
+    return new CellProcessor[] {
+      new NotNull(), // Full Name
+      new NotNull() // Email address
+    };
+  }
 }
